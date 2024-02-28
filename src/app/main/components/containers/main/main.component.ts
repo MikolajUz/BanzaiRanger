8de6790fdf78592addb1f501fca5547d04e4a063
MikolajUz <muzialko@gmail.com>
2024-02-28 12:59:46 +0100

    Deploy to GitHub Pages

--- src/app/main/components/containers/main/main.component.ts
+++ src/app/main/components/containers/main/main.component.ts
@@ -8,12 +8,9 @@ import { Component } from '@angular/core';
 export class MainComponent {
   displayedImage: string = '';
   title = 'Def vs 2bb';
-  columnNames = ['herovill', 'EP', 'MP', 'CO', 'BTN', 'SB'];
+  columnNames = ['herovill', 'MP', 'CO', 'BTN', 'SB'];
   tableData = [
-    {
-      herovill: 'MP',
-      EP: { path: 'assets/ranges/test2.png' },
-    },
+ 
     {
       herovill: 'CO',
       EP: { path: '../../../../../assets/ranges/test2.png' },
@@ -44,10 +41,9 @@ export class MainComponent {
   ];
 
   title2 = 'Open Raise 10bb all';
-  columnNames2 = ['EP', 'MP', 'CO', 'BTN', 'SB'];
+  columnNames2 = [ 'MP', 'CO', 'BTN', 'SB'];
   tableData2 = [
     {
-      EP: { path: 'assets/ranges/OR/OR_EP_10_10_10_10_10_10.png' },
       MP: { path: 'assets/ranges/OR/OR_MP_10_10_10_10_10.png' },
       CO: { path: 'assets/ranges/OR/OR_CO_10_10_10_10.png' },
       BTN: { path: 'assets/ranges/OR/OR_BTN_10_10_10.png' },
