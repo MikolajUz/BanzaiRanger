7844e505412e546b93f0b79902018c501ce3b1a8
MikolajUz <muzialko@gmail.com>
2024-02-29 10:15:59 +0100

    data update

--- src/app/main/components/containers/main/main.component.ts
+++ src/app/main/components/containers/main/main.component.ts
@@ -1,38 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface position {
+  path: string;
+}
+
+interface chartData {
+  title: string;
+  columnNames: string[];
+  tableData: {
+    herovill: string;
+    MP: position;
+    CO: position;
+    BTN: position;
+    SB: position;
+  }[];
+}
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+  ngOnInit(): void {
+    console.log('table', this.table[0].title);
+  }
   displayedImage: string = '';
-  title = 'Def vs 2bb';
   columnNames = ['herovill', 'MP', 'CO', 'BTN', 'SB'];
+
+  title = 'Def vs 2bb';
+
   tableData = [
- 
     {
       herovill: 'CO',
-      EP: { path: '../../../../../assets/ranges/test2.png' },
+
       MP: { path: '../../../../../assets/ranges/test2.png' },
     },
     {
       herovill: 'BTN',
-      EP: { path: '../../../../../assets/ranges/test2.png' },
+
       MP: { path: '../../../../../assets/ranges/test2.png' },
       CO: { path: '../../../../../assets/ranges/test2.png' },
     },
 
     {
       herovill: 'SB',
-      EP: { path: '../../../../../assets/ranges/test2.png' },
+
       MP: { path: '../../../../../assets/ranges/test2.png' },
       CO: { path: '../../../../../assets/ranges/test2.png' },
       BTN: { path: '../../../../../assets/ranges/test.png' },
     },
     {
       herovill: 'BB',
-      EP: { path: '../../../../../assets/ranges/test2.png' },
+
       MP: { path: '../../../../../assets/ranges/test2.png' },
       CO: { path: '../../../../../assets/ranges/test2.png' },
       BTN: { path: '../../../../../assets/ranges/test2.png' },
@@ -41,13 +61,49 @@ export class MainComponent {
   ];
 
   title2 = 'Open Raise 10bb all';
-  columnNames2 = [ 'MP', 'CO', 'BTN', 'SB'];
-  tableData2 = [
+  columnNames2 = ['MP', 'CO', 'BTN', 'SB'];
+  tableData2 = [{}];
+
+  table: chartData[] = [
     {
-      MP: { path: 'assets/ranges/OR/OR_MP_10_10_10_10_10.png' },
-      CO: { path: 'assets/ranges/OR/OR_CO_10_10_10_10.png' },
-      BTN: { path: 'assets/ranges/OR/OR_BTN_10_10_10.png' },
-      SB: { path: 'assets/ranges/OR/OR_SB_10_10.png' },
+      title: 'Open Raise',
+      columnNames: ['herovill', 'MP', 'CO', 'BTN', 'SB'],
+      tableData: [
+        {
+          herovill: '10 bb',
+          MP: { path: 'assets/ranges/OR/OR_MP_10_10_10_10_10.png' },
+          CO: { path: 'assets/ranges/OR/OR_CO_10_10_10_10.png' },
+          BTN: { path: 'assets/ranges/OR/OR_BTN_10_10_10.png' },
+          SB: { path: 'assets/ranges/OR/OR_SB_10_10.png' },
+        },
+        {
+          herovill: '12 bb',
+          MP: { path: '-' },
+          CO: { path: '-' },
+          BTN: { path: 'assets/ranges/OR/12bb_BU_open.png' },
+          SB: { path: 'assets/ranges/OR/12bb_SB_open.png' },
+        },
+      ],
+    },
+    {
+      title: 'Def vs AI on BB',
+      columnNames: ['herovill', 'MP', 'CO', 'BTN', 'SB'],
+      tableData: [
+        {
+          herovill: '10 bb',
+          MP: { path: '-' },
+          CO: { path: '-' },
+          BTN: { path: 'assets/ranges/DefvsAllin/DEF_BBvsBu_Allin.png' },
+          SB: { path: 'assets/ranges/DefvsAllin/DEF_BBvsSB_allin.png' },
+        },
+        {
+          herovill: '12 bb',
+          MP: { path: '-' },
+          CO: { path: '-' },
+          BTN: { path: '-' },
+          SB: { path: '-' },
+        },
+      ],
     },
   ];
 }
