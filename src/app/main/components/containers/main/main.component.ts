fcae07097bca19012f7686f872d9c0c6bf9017f1
MikolajUz <muzialko@gmail.com>
2024-03-02 08:36:24 +0100

    Deploy to GitHub Pages

--- src/app/main/components/containers/main/main.component.ts
+++ src/app/main/components/containers/main/main.component.ts
@@ -40,20 +40,20 @@ export class MainComponent implements OnInit {
           BTN: { path: 'assets/ranges/OR/OR_BTN_10_10_10.png' },
           SB: { path: 'assets/ranges/OR/OR_SB_10_10.png' },
         },
-        {
-          herovill: '12 bb',
-          MP: { path: 'assets/ranges/OR/12bb_MP_open.png' },
-          CO: { path: 'assets/ranges/OR/12bb_CO_open.png' },
-          BTN: { path: 'assets/ranges/OR/12bb_BU_open.png' },
-          SB: { path: 'assets/ranges/OR/12bb_SB_open.png' },
-        },
-        {
-          herovill: '14 bb',
-          MP: { path: 'assets/ranges/OR/14bb/MP_open_14bb.png' },
-          CO: { path: 'assets/ranges/OR/14bb/CO_open_14bb.png' },
-          BTN: { path: 'assets/ranges/OR/14bb/BU_open_14bb.png' },
-          SB: { path: 'assets/ranges/OR/14bb/SB_open_14bb.png' },
-        },
+        // {
+        //   herovill: '12 bb',
+        //   MP: { path: 'assets/ranges/OR/12bb_MP_open.png' },
+        //   CO: { path: 'assets/ranges/OR/12bb_CO_open.png' },
+        //   BTN: { path: 'assets/ranges/OR/12bb_BU_open.png' },
+        //   SB: { path: 'assets/ranges/OR/12bb_SB_open.png' },
+        // },
+        // {
+        //   herovill: '14 bb',
+        //   MP: { path: 'assets/ranges/OR/14bb/MP_open_14bb.png' },
+        //   CO: { path: 'assets/ranges/OR/14bb/CO_open_14bb.png' },
+        //   BTN: { path: 'assets/ranges/OR/14bb/BU_open_14bb.png' },
+        //   SB: { path: 'assets/ranges/OR/14bb/SB_open_14bb.png' },
+        // },
         {
           herovill: '16 bb',
           MP: { path: 'assets/ranges/OR/16bb/MP_open_16bb.png' },
@@ -61,13 +61,13 @@ export class MainComponent implements OnInit {
           BTN: { path: 'assets/ranges/OR/16bb/BU_open_16bb.png' },
           SB: { path: 'assets/ranges/OR/16bb/SB_open_16bb.png' },
         },
-        {
-          herovill: '18 bb',
-          MP: { path: 'assets/ranges/OR/18bb/MP_open_18bb.png' },
-          CO: { path: 'assets/ranges/OR/18bb/CO_open_18bb.png' },
-          BTN: { path: 'assets/ranges/OR/18bb/BU_open_18bb.png' },
-          SB: { path: 'assets/ranges/OR/18bb/SB_open_18bb.png' },
-        },
+        // {
+        //   herovill: '18 bb',
+        //   MP: { path: 'assets/ranges/OR/18bb/MP_open_18bb.png' },
+        //   CO: { path: 'assets/ranges/OR/18bb/CO_open_18bb.png' },
+        //   BTN: { path: 'assets/ranges/OR/18bb/BU_open_18bb.png' },
+        //   SB: { path: 'assets/ranges/OR/18bb/SB_open_18bb.png' },
+        // },
         {
           herovill: '20 bb',
           MP: { path: 'assets/ranges/OR/20bb/MP_open_20bb.png' },
@@ -83,17 +83,17 @@ export class MainComponent implements OnInit {
       tableData: [
         {
           herovill: '10 bb',
+          MP: { path: 'assets/ranges/DefvsAllin/DEF_BBvsMP_allin_10bb.png' },
+          CO: { path: 'assets/ranges/DefvsAllin/DEF_BBvsCO_allin_10bb.png' },
+          BTN: { path: 'assets/ranges/DefvsAllin/DEF_BBvsBTN_allin_10bb.png' },
+          SB: { path: 'assets/ranges/DefvsAllin/DEF_BBvsSB_allin_10bb.png' },
+        },
+        {
+          herovill: '12 bb',
           MP: { path: 'assets/ranges/DefvsAllin/DEF_BBvsMP_allin.png' },
           CO: { path: 'assets/ranges/DefvsAllin/DEF_BBvsCO_allin.png' },
           BTN: { path: 'assets/ranges/DefvsAllin/DEF_BBvsBu_Allin.png' },
           SB: { path: 'assets/ranges/DefvsAllin/DEF_BBvsSB_allin.png' },
-        },
-        {
-          herovill: '12 bb',
-          MP: { path: '-' },
-          CO: { path: '-' },
-          BTN: { path: '-' },
-          SB: { path: '-' },
         },
         {
           herovill: '14 bb',
